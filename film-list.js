5583887f16875c8e33742758b39d95423cc553a7
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2024-12-08 21:58:46 +0300

    first commit

---
+++ film-list.js
@@ -0,0 +1,19 @@
+// Kullanıcıdan favori filmlerini al ve bir array'de tut.
+// Filmleri alfabetik sıraya dizip ekrana yazdır.
+
+
+let filmler = [
+
+];
+
+for (let i = 1; i <= 5; i++) {
+    let favoriFilmler = prompt("Favori filmlerinizi giriniz.");
+    film = [
+        favoriFilmler
+    ]
+    filmler.sort();
+    filmler.push(film);
+
+
+}
+console.log(filmler);
